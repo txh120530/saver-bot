bec4b7f3842379d2eb9ded7a771ceaf529a1c468
Travis Harvey <txh120530@gmail.com>
2022-03-20 21:21:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -45,10 +45,6 @@ const db = mysql.createPool({
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
 
-  // Express serve up index.html file if it doesn't recognize route
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 
 
 function handleDisconnect() {
@@ -147,6 +143,11 @@ function handleDisconnect() {
     res.json(res.paginatedResults);
   });
 
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 
 
   
