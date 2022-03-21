4693f0e16cf629bb242e1941757aa9f0568375d2
Travis Harvey <txh120530@gmail.com>
2022-03-20 20:15:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,31 @@ const slackClient = new WebClient(slackToken);
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 
+
+function handleDisconnect() {
+    console.log('handleDisconnect()');
+    db = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+    db.connect(function(err) {              // The server is either down
+    if(err) {                                      // or restarting (takes a while sometimes).
+        console.log(' Error when connecting to db:', err);
+        setTimeout(handleDisconnect, 1000);         // We introduce a delay before attempting to reconnect,
+    }                                               // to avoid a hot loop, and to allow our node script to
+    });                                             // process asynchronous requests in the meantime.
+                                                    // If you're also serving http, display a 503 error.
+
+    db.on('  Database Error', function(err) {
+        console.log('db error: ' + err.code, err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                       // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                // server variable configures this)
+        }
+    });
+
+}
+
+
 slackEvents.on('app_mention', (event) => {
   console.log(`Got message from user ${event.user}: ${event.text}`);
   console.log(event);
@@ -168,7 +193,12 @@ catch (error) {
 
 
   app.get('/comments', paginatedResults(), (req, res) => {
-    console.log(res.paginatedResults);
+    db.connect(function(err) {
+            if(err) {
+                console.log('Connection is asleep (time to wake it up): ', err);
+                setTimeout(handleDisconnect, 1000);
+            }
+            });
     res.json(res.paginatedResults);
   });
 
