0e897e89c6722486c7e353d258091feeb47902bd
Travis Harvey <txh120530@gmail.com>
2022-03-20 23:39:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -58,13 +58,13 @@ function handleDisconnect() {
     db.connect(function(err) {              // The server is either down
     if(err) {                                      // or restarting (takes a while sometimes).
         console.log(' Error when connecting to db:', err);
-        setTimeout(handleDisconnect, 1000);         // We introduce a delay before attempting to reconnect,
+        setTimeout(handleDisconnect, 2000);         // We introduce a delay before attempting to reconnect,
     }                                               // to avoid a hot loop, and to allow our node script to
     });                                             // process asynchronous requests in the meantime.
                                                     // If you're also serving http, display a 503 error.
 
-    db.on('Database Error', function(err) {
-        console.log('db error: ' + err.code, err);
+    db.on('error', function(err) {
+        console.log('db error: ' + err);
         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
             handleDisconnect();                       // lost due to either server restart, or a
         } else {                                      // connnection idle timeout (the wait_timeout
